b4be7b8407be9f62fd70d6775f659a6ed5a3652b
tranthinhsysops <105266105+tranthinhsysops@users.noreply.github.com>
2022-05-10 13:39:40 +0700

    Update renew-license.sh

--- renew-license.sh
+++ renew-license.sh
@@ -2,7 +2,7 @@
 /usr/sbin/ifdown lo:100
 rm -f /tmp/license.key
 rm -f /usr/local/directadmin/conf/license.key
-/usr/bin/wget -O /tmp/license.key.gz https://github.com/tranthinhsysops/DirectAdmin/blob/main/license.key.gz
+/usr/bin/wget -O /tmp/license.key.gz https://github.com/tranthinhsysops/DirectAdmin/raw/main/license.key.gz
 /usr/bin/gunzip /tmp/license.key.gz
 mv /tmp/license.key /usr/local/directadmin/conf/
 chmod 600 /usr/local/directadmin/conf/license.key
